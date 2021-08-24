e80a9b2cc2954958e5242bb6b78c1dc320ae1d0f
ch3cknull <ch3cknull@outlook.com>
2021-08-24 11:30:40 +0800

    fix: add column uid in table reply

--- cnki.sql
+++ cnki.sql
@@ -2,10 +2,10 @@ drop table if exists user_dynamic;
 create table user_dynamic
 (
     dynamic_id bigint comment '动态唯一id',
-    type_id    int comment '动态类型, 用于获取评论',
+    type_id    int comment '动态类型, 用于获取评论, 区分视频/',
     oid        bigint comment '动态对象id，用于获取评论',
     ctime      int comment '动态创建时间',
-    uid        int comment '动态uid',
+    uid        int comment '视频/专栏/动态 拥有者(UP主)uid',
     status     int comment '评论爬取状态，0: 未爬取， 1: 至少爬取完成过一次',
     primary key (dynamic_id)
 ) DEFAULT CHARACTER SET = utf8mb4;
@@ -22,12 +22,13 @@ drop table if exists reply;
 create table reply
 (
     rpid       bigint comment '回复id',
-    type_id    int,
-    dynamic_id bigint,
-    mid        int comment '成员id',
+    type_id    int comment '动态类型,用于区分视频/专栏/动态'
+    dynamic_id bigint comment '适用于动态的评论区',
+    uid        int comment '视频/专栏/动态 拥有者(UP主)uid',
+    mid        int comment '评论发布者id',
     oid        bigint comment '源稿件id',
     ctime      int comment '创建时间',
-    m_name     varchar(255) comment '昵称',
+    m_name     varchar(255) comment '评论发布者昵称',
     content    text comment '评论内容',
     like_num   int,
     primary key (rpid)
